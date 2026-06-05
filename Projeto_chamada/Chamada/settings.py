8e36117bebea5578ca41a1f5a377747902d98918
igor briano <igor.briano1998@gmail.com>
2026-06-05 00:33:29 -0300

    ainda resolvendo css

--- Projeto_chamada/Chamada/settings.py
+++ Projeto_chamada/Chamada/settings.py
@@ -142,8 +142,13 @@ USE_I18N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "Projeto_chamada/core/static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGOUT_REDIRECT_URL = 'login'
 
